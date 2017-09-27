868a928eb0633a466549f4630e0fb449947cfa05
Caio Caly <caio.caly@gmail.com>
2017-09-27 11:04:12 -0300

    Achievements tipo 1 funcionando

--- achievements_list.js
+++ achievements_list.js
@@ -1,8 +1,9 @@
 class Achievement {
-	constructor (type, title, subtitle, condition) {
+	constructor (type, title, subtitle, howToGet, condition) {
 		this.type = type;
 		this.title = title;
 		this.subtitle = subtitle;
+		this.howToGet = howToGet;
 		this.condition = this.indexDrags(condition);
 		this.achieved = false;
 	}
@@ -29,14 +30,43 @@ class Achievement {
 
 function checkAchievements () {
 	achievementsList.forEach(function(item, index){
-		
-	});
-}
+		switch (item.type) {
+			case 1:
+			if (item.achieved === false) {
+				for (i = 0; i < item.condition.length; i++ ) {
+					if (listOfGuessedIndexes.indexOf(item.condition[i]) === -1){
+						break;
+					} else if (i === (item.condition.length - 1)) {
+						item.achieved = true;
+					}
+				}
 
+				if (item.achieved === true) {alert('Achievement unlocked!')};
+				break;
+			}//fim do if do case 1
+
+			case 2:
+
+		} //fim do switch todo
+	} //fim da função contida no foreach
+); //fim do  foreach 
+} //fim da função
 
 
 function createAchievements () {
 	achievementsList = [ 
-	new Achievement ("all", "JIMI JIMI AJA", "Só drags que contém 'aja' no nome kkkk", ['acid betty', 'adore']),
+	new Achievement (
+		1,
+		"Girl look how orange you fucking look", 
+		"Some healthy sisterly rivalry", 
+		"Guess both Coco Montrese and ALyssa Edwards", 
+		['coco', 'alyssa edwards']),
+
+	new Achievement (
+		1,
+		"It came from another planet!", 
+		"Mother has INDEED arrived", 
+		'Guess "Ornatia"', 
+		['Ornatia'])
 ]
 }
